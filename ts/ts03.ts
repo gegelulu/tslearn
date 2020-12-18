d253edb5de97c3771d58ecf4b5bc411d2b0ec52f
gelu-dub <gegelulu1899@gmail.com>
2020-12-18 22:59:37 +0800

    try

--- ts/ts03.ts
+++ ts/ts03.ts
@@ -10,3 +10,16 @@ someThing = '7';
 let everything;
 everything = 9;
 everything = 'some';
+
+let someY: string | number;
+someY = 'eef';
+someY = 7;
+let someN: string | boolean;
+someN = true;
+
+function getL(someY: string | number): any{
+    return someY;
+    // return someY.toString();
+}
+
+
